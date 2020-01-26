de28a5478f224f92b8c2ae606ac389547a507345
evgeniyPP <amenat12@mail.ru>
2020-01-26 15:23:16 +0300

    no-http

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getIMDB = async (input: string) => {
   const query = input.replace(/\s/g, '+');
-  const res = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=4a61c6f&t=${query}`);
+  const res = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=4a61c6f&t=${query}`);
   if (res.status === 200) {
     return {
       title: res.data.Title,
