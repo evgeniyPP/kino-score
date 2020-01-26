2467bb2d9d7b78934765c6f55ac7a0a2ee747aea
evgeniyPP <amenat12@mail.ru>
2020-01-26 08:48:39 +0300

    firebase functions

---
+++ functions/index.js
@@ -0,0 +1,72 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+const puppeteer = require('puppeteer');
+
+const scrapeKinopoisk = async query => {
+  const url = `https://www.kinopoisk.ru/index.php?kp_query=${encodeURIComponent(query)}`;
+  const res = await fetch(url);
+  const html = await res.text();
+  const $ = cheerio.load(html);
+  return {
+    name: $('.most_wanted .info .name a').text(),
+    year: $('.most_wanted .info .name .year').text(),
+    englishName: $('.most_wanted .info .name + .gray')
+      .text()
+      .replace(/,.+/, ''),
+    rating: $('.most_wanted .rating').text()
+  };
+};
+
+exports.kinopoisk = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const data = await scrapeKinopoisk(req.body.query);
+    res.send(data);
+  });
+});
+
+const scrapeTomatoes = async (title, year) => {
+  const query = title.replace(/\s/g, '%20');
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+  const page = await browser.newPage();
+
+  await page.goto(`https://www.rottentomatoes.com/search/?search=${query}`);
+
+  const data = await page.evaluate(() => {
+    const ratings = Array.from(
+      document.querySelectorAll('.search__results-movies .search__results-item-tomatometer span')
+    )
+      .map(rating => rating.textContent.replace(/\s|%/g, ''))
+      .filter(item => item !== '');
+    const names = Array.from(
+      document.querySelectorAll('.search__results-movies .search__results-item-info-name')
+    ).map(name => name.textContent);
+    const years = Array.from(
+      document.querySelectorAll('.search__results-movies .search__results-item-info-year')
+    ).map(year => year.textContent.replace(/\s|\(|\)/g, ''));
+
+    return names.map((name, index) => {
+      return {
+        name: name,
+        year: years[index],
+        rating: ratings[index]
+      };
+    });
+  });
+
+  const result = data.filter(film => film.name === title && film.year === year)[0];
+
+  await browser.close();
+  return result;
+};
+
+exports.tomatoes = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const data = await scrapeTomatoes(req.body.title, req.body.year);
+    res.send(data);
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { getIMDB } from './api';
+import styles from './styles/App.module.scss';
 
 const App: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [ratings, setRatings] = useState({
+    kinopoisk: { title: null, score: null },
+    imdb: { title: null, score: null },
+    meta: { title: null, score: null },
+    tomatoes: { title: null, score: null }
+  });
+  const [result, setResult] = useState('');
+
+  const handleClick = async () => {
+    const data = await getIMDB(input);
+    if (data) {
+      setRatings((prev: any) => ({
+        ...prev!,
+        imdb: { title: data.title, score: data.imdb },
+        meta: { title: data.title, score: data.meta }
+      }));
+      setResult(calculate(data.imdb, data.meta));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <h1>Введите название фильма:</h1>
+      <input type="text" value={input} onChange={e => setInput(e.currentTarget.value)} />
+      <button onClick={handleClick}>Посчитать</button>
+      {ratings.kinopoisk.score && (
+        <h2>
+          Кинопоиск: {ratings.kinopoisk.score}{' '}
+          <span className={styles.gray}>('{ratings.kinopoisk.title}')</span>
+        </h2>
+      )}
+      {ratings.imdb.score && (
+        <h2>
+          IMDB: {ratings.imdb.score} <span className={styles.gray}>('{ratings.imdb.title}')</span>
+        </h2>
+      )}
+      {ratings.meta.score && (
+        <h2>
+          Metacritics: {ratings.meta.score}{' '}
+          <span className={styles.gray}>('{ratings.meta.title}')</span>
+        </h2>
+      )}
+      {ratings.tomatoes.score && (
+        <h2>
+          Rotten Tomatoes: {ratings.tomatoes.score}{' '}
+          <span className={styles.gray}>('{ratings.tomatoes.title}')</span>
+        </h2>
+      )}
+      {result && <h2>Общий счет: {result}</h2>}
+      <footer>
+        Формула подсчета: ( Кинопоиск + IMDB + (( Metacritics + Rotten Tomatoes ) / 2) ) / 3
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
+
+function calculate(imdb: string, meta: string) {
+  const rating = (Number(imdb) + Number(meta) / 10) / 2;
+  return rating.toFixed(2);
+}
